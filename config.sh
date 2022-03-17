8d379730cae9d986051d1b512f54d22f4d715d0a
Jprimero15 <jprimero155@gmail.com>
2022-03-17 10:53:36 +0800

    init ofox olive
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_11.0"
+export DT_LINK="https://github.com/Jprimero15/android_device_xiaomi_olive -b fox_11"
 
-export DEVICE="beryllium"
+export DEVICE="olive"
 export OEM="xiaomi"
 export TARGET="recoveryimage"
 
