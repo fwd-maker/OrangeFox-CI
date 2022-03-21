8784bdb113bf20c132e071284c3c0d8a478d4b05
fwd-maker <65204598+fwd-maker@users.noreply.github.com>
2022-03-21 10:17:02 +0000

    Use fox 9.0 branch

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_11.0"
-export DT_LINK="https://github.com/Jprimero15/android_device_xiaomi_olive -b fox_11"
+export FOX_BRANCH="fox_9.0"
+export DT_LINK="https://github.com/Jprimero15/android_device_xiaomi_olive -b fox_9.0"
 
 export DEVICE="olive"
 export OEM="xiaomi"
